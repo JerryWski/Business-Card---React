4f9b9281c91fa4d4573e0c2e7a20ee11f6528d9e
JerryWski <jarek_wasilewski@yahoo.com>
2023-01-29 14:06:26 +0100

    setting up for new app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,1 @@
 import styles from "./App.module.css";
-import NavBar from "./components/NavBar";
-import MainContnet from "./components/MainContent";
-
-function App() {
-  return (
-    <div className={styles.container}>
-      <NavBar />
-      <MainContnet/>
-    </div>
-  );
-}
-
-export default App;
