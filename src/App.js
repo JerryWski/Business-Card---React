502e36f66ffa52a7f8bc64541470b8e6acf0e2d8
JerryWski <jarek_wasilewski@yahoo.com>
2023-02-01 23:13:55 +0100

    new branch test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
+import NavBar from "./components/NavBar";
 import styles from "./App.module.css";
-import About from "./components/About";
-import Footer from "./components/Footer";
-import ImagePhoto from "./components/ImagePhoto";
-import InfoPerson from "./components/InfoModule";
-import Interest from "./components/Interest";
+
 
 const App = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.card}>
-        <ImagePhoto />
-        <div className={styles.wrapper}>
-          <InfoPerson />
-          <About />
-          <Interest />
-        <Footer />
-        </div>
-      </div>
+    <div className={styles.appContainer}>
+    <NavBar />
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import styles from "./NavBar.module.css";
+
+const NavBar = () => {
+    return (
+        <div>
+            <nav className={styles.navContainer}>
+                Logo
+            </nav>
+        </div>
+    )
+}
+
+export default NavBar;
