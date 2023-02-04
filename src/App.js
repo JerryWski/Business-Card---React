74ea0b914ad383a209d666bf48a55fad584ecf05
JerryWski <jarek_wasilewski@yahoo.com>
2023-02-05 00:53:22 +0100

    pass objects as props

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
 import NavBar from "./components/NavBar";
 import styles from "./App.module.css";
-import HeroSection from "./components/HeroSection";
+// import HeroSection from "./components/HeroSection";
 import Card from "./components/Card";
 import personalData from "./data";
 // import Joke from "./components/Joke";
 // import jokesData from "./jokesData";
 
 const App = () => {
-  // const jokeElements = jokesData.map(joke => {
-  //   return <Joke
-  //         setup={joke.setup}
-  //         punchLine= {joke.punchline}
-  //   />
-  // })
+  
   const dataElements = personalData.map((data) => {
     return (
       <Card
-      key= {data.id}
-        img={data.coverImg}
-        rating={data.stats.rating}
-        reviewCountry={data.stats.reviewCount}
-        location={data.location}
-        title={data.title}
-        price={data.price}
+        key={data.id}
+        data={data}
       />
     );
   });
@@ -32,9 +22,7 @@ const App = () => {
       <div className={styles.app_container}>
         <NavBar />
         {/* <HeroSection /> */}
-        <section className={styles.card_list}>
-        {dataElements}
-        </section>
+        <section className={styles.card_list}>{dataElements}</section>
       </div>
     </div>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,27 +1,31 @@
 import styles from "./Card.module.css";
 
 const Card = (props) => {
+  
+  let badgeText;
+  if (props.openSpots === 0) {
+    badgeText = "SOLD OUT"
+  }else if (props.location === 'Online'){
+    badgeText = 'ONLINE'
+  }
   return (
     <div className={styles.card}>
-      <img
-        className={styles.card__person}
-        src={props.img}
-        alt=""
-      />
+      {badgeText && <div className={styles.card_badge}>{badgeText}</div>}
+      <img className={styles.card__person} src={props.data.coverImg} alt="" />
       <div className={styles.card__stats}>
         <img
           className={styles.card__star}
           src={require("../images/star.png")}
           alt="star-logo"
         />
-        <span>{props.rating}</span>
-        <span className={styles.gray}> {props.reviewCountry} • </span>
-        <span className={styles.gray}>{props.location}</span>
+        <span>{props.data.stats.rating}</span>
+        <span className={styles.gray}> ({props.data.stats.reviewCount}) • </span>
+        <span className={styles.gray}> {props.data.location}</span>
       </div>
       <div className={styles.card__bio}>
-        <p className={styles.card_title}>{props.title}</p>
+        <p className={styles.card_title}>{props.data.title}</p>
         <p>
-          <span className={styles.bold}>From ${props.price}</span> / person
+          <span className={styles.bold}>From ${props.data.price}</span> / person
         </p>
       </div>
     </div>
